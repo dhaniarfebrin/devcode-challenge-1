3d60d3dc6cde4f5b00a130be909915f96dce0348
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2024-03-24 18:45:16 +0700

    initial commit

---
+++ src/components/WelcomeCard.svelte
@@ -0,0 +1,10 @@
+<div>
+  <h1>Selamat datang di Devcode!</h1>
+  <h2>
+    Silahkan klik tombol di bawah ini untuk melihat hasil akhir dari perubahan
+    yang kamu lakukan.
+  </h2>
+ <div data-cy="router-button">
+    <a class="router__btn" href="/hello">Lihat hasil</a>
+  </div> 
+</div>
